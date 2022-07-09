0fa3195f79402804d4cddad08b9a32eb63bfd96b
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-07-09 16:51:05 +0300

    asynchronous function call

--- index.js
+++ index.js
@@ -47,7 +47,14 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 };
 
 // commander
-invokeAction(argv);
+invokeAction(argv); const start = async (argv) => {
+  try {
+    await invokeAction(argv);
+  } catch (error) {
+    console.log(error);
+  }
+};
+start(argv);
 
 //list
 // invokeAction({ action: 'list' });
